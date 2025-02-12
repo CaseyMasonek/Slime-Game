7731d400edf64f471979ffd17854821c5c3f2bb0
Casey Masonek <cmmasonek@gmail.com>
2025-02-12 11:21:37 -0800

    do NOT let harrison touch you

--- Assets/Scripts/Controllers/BasicEnemyController.cs
+++ Assets/Scripts/Controllers/BasicEnemyController.cs
@@ -6,17 +6,29 @@ using UnityEngine;
 public class BasicEnemyController : MonoBehaviour, IMovementController
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float flipThreshold;
 
     private Direction _direction;
     private Ground _ground;
+	private Rigidbody2D _body;
 
     private void Start()
     {
-        
+        _direction = GetComponent<Direction>();
+        _body = GetComponent<Rigidbody2D>();
+        _ground = GetComponent<Ground>();
+    }
+
+    private void Update()
+    {
+        if (_ground.wall != 0 || Mathf.Abs(_body.velocity.x) < flipThreshold)
+        {
+            _direction.Flip();
+        }
     }
 
     public float GetMovement()
     {
-        return 2;
+        return _direction.AsSign();
     }
 }
